49cfa4d4bdfba65d0b0890d6fbc1932565404a76
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-07-26 21:47:23 -0700

    Updated the fruit model to allow image uploads
    File field should be used to addFruit image for the specific fruit

--- src/fruit_app/models.py
+++ src/fruit_app/models.py
@@ -6,5 +6,6 @@ class Fruit(models.Model):
     name =  models.CharField(blank= False, max_length = 25) 
     description = models.TextField(blank= False, null= False)
     price = models.DecimalField(blank=False, decimal_places= 2, max_digits= 6)
+    image = models.FileField(upload_to= "fruit_app/img/fruits", blank= True)
 
     
