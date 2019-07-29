47017c1620275dc78cadbde9ebc0ab698653bae1
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-07-28 21:10:49 -0700

    Added imageurl field to hold urls for fruit images

--- src/fruit_app/models.py
+++ src/fruit_app/models.py
@@ -6,6 +6,7 @@ class Fruit(models.Model):
     name =  models.CharField(blank= False, max_length = 25) 
     description = models.TextField(blank= False, null= False)
     price = models.DecimalField(blank=False, decimal_places= 2, max_digits= 6)
-    image = models.FileField(upload_to= "fruit_app/img/fruits", blank= True)
+    image = models.ImageField(upload_to = "media", blank = True)
+    imageurl = models.URLField(blank = True)
 
     
